b2d8366ef05138549119b0a064bc77a0f670d6c2
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 18:47:31 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,16 @@
 def nyc_pigeon_organizer(data)
-  organized = {}
-   data.each do |trait, options|
-    options.each do |option, pigeons|
-      pigeons.each do |pigeon|
-        organized[pigeon] ||= {}
-        if trait == :gender || trait == :lives
-          organized[pigeon][trait] = option.to_s
-        else
-          organized[pigeon][trait] ||= []
-          organized[pigeon][trait] << option.to_s
-        end
-      end
-    end
-  end
-   organized
-end 
+   data.inject({}) do |memo, trait_array|
+     trait_array[1].each do |option, pigeons|
+       pigeons.each do |pigeon|
+         memo[pigeon] ||= {}
+         if trait_array[0] == :color
+           memo[pigeon][trait_array[0]] ||= []
+           memo[pigeon][trait_array[0]] << option.to_s
+         else
+           memo[pigeon][trait_array[0]] = option.to_s
+         end
+       end
+     end
+     memo
+   end
+ end
