4abef2c857739f04e8a176a445d0acd5a6168a94
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 18:53:52 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,28 @@
-def nyc_pigeon_organizer(data)
-#   organized = {}	  organized = {}
- #   data.each do |trait, options|	  data.each do |trait, options|
-#     options.each do |option, pigeons|	    options.each do |option, pigeons|
-#       pigeons.each do |pigeon|	      pigeons.each do |pigeon|
-#         organized[pigeon] ||= {}	        organized[pigeon] ||= {}
-#         if trait == :gender || trait == :lives	        organized[pigeon][trait] ||= []
-#           organized[pigeon][trait] = option.to_s	        organized[pigeon][trait] << option.to_s
-#         else	      end
-#           organized[pigeon][trait] ||= []	    end
-#           organized[pigeon][trait] << option.to_s	  end
-#         end	
-#       end	
-#     end	
-#   end	
- #   organized	  organized
-# end	end
+def nyc_pigeon_organizer(data)	# def nyc_pigeon_organizer(data)
+  organized = {}	#   organized = {}
+   data.each do |trait, options|	
+    options.each do |option, pigeons|	
+      pigeons.each do |pigeon|	
+        organized[pigeon] ||= {}	
+        organized[pigeon][trait] ||= ''	
+        if organized[pigeon][trait].size > 0	
+          organized[pigeon][trait] = (organized[pigeon][trait].split << option.to_s).flatten	
+        else	
+          organized[pigeon][trait] = option.to_s	
+        end	
+      end	
+    end	
+     array = false	
+    organized.keys.each do |pigeon|	
+      if organized[pigeon][trait].is_a?(Array)	
+        array = true	
+      end	
+    end	
+     if array	
+      organized.each do |pigeon, traits|	
+        if !organized[pigeon][trait].is_a?(Array)	
+          organized[pigeon][trait] = [organized[pigeon][trait]]	
+        end	
+      end	
+    end	
+   end
